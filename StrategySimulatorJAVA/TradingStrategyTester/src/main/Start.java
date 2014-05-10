9476ed9c02ef5104dd06095fe1fbe034da15e9f1
Carina Huchler <carina.huchler@gmail.com>
2014-05-10 19:28:00 +0200

    Copy to try out stuff

--- StrategySimulatorJAVA/TradingStrategyTester/src/main/Start.java
+++ StrategySimulatorJAVA/TradingStrategyTester/src/main/Start.java
@@ -4,10 +4,13 @@ import datacollection.CurrencyCourseCreator;
 import datacollection.CurrencyCourseOHLC;
 import datacollection.FormatMT4OHLC;
 import indicators.SMA;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import simulation.CopyOfStrategySimulation;
 import simulation.StrategySimulation;
 import strategies.AbstractStrategy;
 import strategies.SimpleTestStrategySMA;
@@ -23,7 +26,7 @@ public class Start {
         CurrencyCourseCreator ccc = new FormatMT4OHLC();
         CurrencyCourseOHLC cc = new CurrencyCourseOHLC();
         try {
-            cc = ccc.getCurrencyCourseFromFile("EURUSD.1.csv", "EURUSD");
+            cc = ccc.getCurrencyCourseFromFile("C:\\Users\\Carina\\Documents\\Dots-And-Boxes\\Operations Research Project\\theFOREXproject\\StrategySimulatorJAVA\\TradingStrategyTester\\EURUSD.1.csv", "EURUSD");
         } catch (IOException ex) {
             Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParseException ex) {
@@ -43,7 +46,7 @@ public class Start {
         int leverage = 5;
         double balance = 50000;
         int timeframeInMinutes = 100;
-        StrategySimulation simulation = new StrategySimulation(simpleTestStrategy, cc, balance, leverage);
+        CopyOfStrategySimulation simulation = new CopyOfStrategySimulation(simpleTestStrategy, cc, balance, leverage);
         simulation.simulateStrategy(timeframeInMinutes);
     }
 }
---
+++ StrategySimulatorJAVA/TradingStrategyTester/src/simulation/CopyOfStrategySimulation.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulation;
+
+import datacollection.CurrencyCourseOHLC;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import strategies.AbstractStrategy;
+
+/**
+ *
+ * @author Moohdyy
+ */
+public class CopyOfStrategySimulation {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private CopyOfTradeManager tm;
+    private AbstractStrategy strategy;
+    private CurrencyCourseOHLC cc;
+    private long actualTime = 0;
+
+    public CopyOfStrategySimulation(AbstractStrategy strategy, CurrencyCourseOHLC cc, double balance) {
+        this.cc = cc;
+        this.strategy = strategy;
+        this.tm = new CopyOfTradeManager(balance);
+    }
+
+    public CopyOfStrategySimulation(AbstractStrategy strategy, CurrencyCourseOHLC cc, double balance, double leverage) {
+        this(strategy, cc, balance);
+        this.tm.setLeverage(leverage);
+    }
+
+    //for this simulation, the strategy just has one order at maximum open and can close a BUY order by returning SELL and vice versa
+    public void simulateStrategy(int windowInMinutes) {
+        long windowInMilliseconds = windowInMinutes * 60 * 1000;
+        List<Trade> trades = new ArrayList<>();
+        for (int index = 0; index < cc.getNumberOfEntries(); index++) {
+            double actualPrice = cc.getClose(index);
+            if (tm.checkForMarginCall()) {
+                System.out.println("MARGIN CALL, simulation stopped.");
+                return;
+            }
+            tm.checkStopLossTakeProfit(actualPrice);
+            this.actualTime = cc.getTimeStamp(index);
+            long windowTime = cc.getTimeStamp(cc.getActualPosition());
+            if (this.actualTime >= windowTime + windowInMilliseconds) { //in our case we can access new course after window
+                cc.setActualPosition(index);
+                System.out.println("Actual price of " + cc.getCurrencyPair() + " : " + actualPrice);
+                trades = strategy.processNewCourse(trades, cc);
+            }
+            tm.processTrades(trades, cc.getBidPrice(index), cc.getClose(index), this.actualTime);
+            printCurrentStats(index);
+        }
+    }
+
+    public void printCurrentStats(int index) {
+        double bid = cc.getBidPrice(index);
+        double ask = cc.getAskPrice(index);
+        System.out.print("________________________________\n" + sdf.format(new Date(actualTime))
+                + ":\n ActualPrice:   " + bid
+                + "\n ActiveTrades:  " + tm.getActiveTradesCount()
+                + ", ClosedTrades: " + tm.getClosedTradesCount()
+                + "\n Balance:       " + Math.round(this.tm.getBalance())
+                + "\n Equity:        " + Math.round(this.tm.getEquity())
+                + "\n UseableMargin: " + "Not Implemented"
+                + "\n Margin:        " + Math.round(this.tm.getFreeMargin())
+                + "\n________________________________________\n"
+        );
+    }
+
+}
